18564878a9b3ce4ddac3f4c82c506017f36b652b
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-01-25 23:00:39 -0500

    Add files via upload

--- online-compiler/backend/executePy.js
+++ online-compiler/backend/executePy.js
@@ -1,9 +1,14 @@
-const { exec } = require("child_process");
+const { exec, spawn, execFile } = require("child_process");
 
 const executePy = (filepath) => {
+  var param2 = 1
+  var param3 = 2
+  var command = "python `${filepath}  ` "  + param2 + " " + param3;
+  console.log("what")
+  console.log(filepath)
   return new Promise((resolve, reject) => {
     exec(
-      `python ${filepath}`,
+      `python3 ./test/twosum.py ${filepath} 2 3 4 5`,
       (error, stdout, stderr) => {
         error && reject({ error, stderr });
         stderr && reject(stderr);
